cf02d2b3a8a8fcc21bf0537498bbfdd5e8a6ce1c
Donald Westland <don@macbook-pro-1t.home>
2021-06-12 11:17:22 -0700

    Add image and public folder support, add README file.

---
+++ README.md
@@ -0,0 +1,37 @@
+# dw-gulp-4-sass-nunjucks
+
+Simple boilerplate for Gulp 4 with Nunjucks, SASS and JavaScript support. Nunjucks allows for easy template inheritance and partials. Hot reloading done by Browsersync.
+
+**DevDependencies:**
+- browser-sync
+- cssnano
+- gulp
+- gulp-concat
+- gulp-imagemin
+- gulp-nunjucks
+- gulp-postcss
+- gulp-sass
+- gulp-svgmin
+- gulp-terser
+- postcss
+---
+## Quick Start
+install:
+```
+git clone https://github.com/dwestland/dw-gulp-4-sass-nunjucks
+```
+Move into directory:
+```
+cd dw-gulp-4-sass-nunjucks
+```
+Install devDependencies
+```
+npm i
+```
+Run Gulp
+```
+gulp
+```
+---
+License
+The code is available under the ISC License (Use it any way you want).
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,26 @@ const { src, dest, watch, series, on } = require('gulp');
 const browsersync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const cssnano = require('cssnano');
+const imagemin = require('gulp-imagemin');
 const nunjucksRender = require('gulp-nunjucks-render');
 const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
+const svgmin = require('gulp-svgmin');
 const terser = require('gulp-terser');
+
+// Public Folder Task (does not delete files)
+function copyPublic(cb) {
+  src("src/public/**/*")
+    .pipe(dest("dist"));
+  cb();
+ }
 
 // Nunjucks Task
 function nunjucks(cb) {
   src("src/pages/**/*.html")
     .pipe(
       nunjucksRender({
-        path: ["src/templates/"]
+        path: ["src/templates", "src/partials"]
       })
     )
     .pipe(dest("dist"));
@@ -28,11 +37,25 @@ function scssTask(){
 }
 
 // JavaScript Task
-function jsTask(){
+function jsTask() {
   return src('src/js/*.js', { sourcemaps: true })
     .pipe(concat('main.js'))
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Image Task
+function imageTask() {
+  return src('src/images/*')
+    .pipe(imagemin())
+    .pipe(dest('dist/images'));
+}
+
+// SVG Task
+function svgTask() {
+  return src('src/svg/*')
+    .pipe(svgmin())
+    .pipe(dest('dist/svg'));
 }
 
 // Browsersync Tasks
@@ -54,13 +77,19 @@ function browsersyncReload(cb){
 function watchTask(){
   watch('src/**/*.html', series(nunjucks, browsersyncReload));
   watch(['src/scss/**/*.scss', 'src/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+  watch('src/public/**/*', series(copyPublic, browsersyncReload));
+  watch('src/images/*', series(imageTask, browsersyncReload));
+  watch('src/svg/*', series(svgTask, browsersyncReload));
 }
 
 // Default Gulp task
 exports.default = series(
+  copyPublic,
   nunjucks,
   scssTask,
   jsTask,
+  imageTask,
+  svgTask,
   browsersyncServe,
   watchTask
 );
